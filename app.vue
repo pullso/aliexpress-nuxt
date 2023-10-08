bcea786d1947b2f03c09d12bceef4f1242acfe45
pullso <pullso.code@gmail.com>
2023-10-08 14:13:44 +0300

    add prisma

--- app.vue
+++ app.vue
@@ -1,7 +1,43 @@
 <template>
-  <div class="fixed z-[-1] bg-[#f2f2f2] w-full h-[100vh]"/>
-    <NuxtPage/>
-
+  <div class="fixed z-[-1] bg-[#f2f2f2] w-full h-[100vh]" />
+  <NuxtPage />
+  <MenuOverlay
+    :class="[
+      {
+        'max-h-[100vh] transition-all duration-200 ease-in visible':
+          userStore.isMenuOverlay,
+      },
+      {
+        'max-h-0 transition-all duration-200 ease-out invisible':
+          !userStore.isMenuOverlay,
+      },
+    ]"
+  />
 </template>
 <script setup lang="ts">
+import { useUserStore } from "~/stores/user";
+const userStore = useUserStore();
+const route = useRoute();
+
+let windowWidth = ref(process.client ? window.innerWidth : 0);
+onMounted(() => {
+  userStore.isLoading = false;
+  window.addEventListener(
+    "resize",
+    () => (windowWidth.value = window.innerWidth)
+  );
+});
+
+watch(windowWidth, () => {
+  if (windowWidth.value >= 768) {
+    userStore.isMenuOverlay = false;
+  }
+});
+
+watch(
+  () => route.fullPath,
+  () => {
+    userStore.isLoading = true;
+  }
+);
 </script>
